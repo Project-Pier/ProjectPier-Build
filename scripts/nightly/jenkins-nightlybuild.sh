414fa425df0a954fba4532e20ce9d82c2ef11c01
Jon DeGenova <github@andrexa.com>
2015-03-08 12:00:25 -0400

    Updated nightly build produces two output files, one containing only the themes and plugins being considered for the next release, another that contains everything (all themes, all plugins, and the tools repo).

--- scripts/nightly/jenkins-nightlybuild.sh
+++ scripts/nightly/jenkins-nightlybuild.sh
@@ -7,6 +7,7 @@
 # Requires git, sed, and zip to be installed
 
 # File system prep
+# File system prep
 rm -rf *
 
 # Checkout the core
@@ -71,5 +72,66 @@ sed -i "s/0.8.8/0.9.0-Nightly-$BUILD_NUMBER/g" ./core/public/install/installatio
 mkdir ./pkg
 cd ./core
 date -u > build_id.txt
+echo Release configuration build, contains only plugins, and themes being considered for the next release >> build_id.txt
 echo Jenkins Build Number $BUILD_NUMBER >> build_id.txt
 zip -r ../pkg/projectpier-master-latest.zip ./*
+
+# Stop here if you only want to build the release configuration.  To also build the "everything" 
+# configuration for testing only, include the section below.  Note that this includes some
+# tools which may not be safe in a production environment and some plug-ins that may not work as
+# well as some themes that do not work very well.
+
+cd ..
+
+# Checkout the Redbase Theme and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Tools.git ./theme-tools
+rm -rf ./theme-tools/.git
+cp -R ./theme-tools/* ./core/
+
+# Checkout the Redbase Theme and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Theme-Redbase.git ./theme-redbase
+rm -rf ./theme-redbase/.git
+cp -R ./theme-redbase/* ./core/
+
+# Checkout the Magellan Theme and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Theme-Magellan.git ./theme-magellan
+rm -rf ./theme-magellan/.git
+cp -R ./theme-magellan/* ./core/
+
+# Checkout the KampPro2 Theme and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Theme-KampPro2.git ./theme-kamppro2
+rm -rf ./theme-kamppro2/.git
+cp -R ./theme-kamppro2/* ./core/
+
+# Checkout the GreenBase Theme and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Theme-Greenbase.git ./theme-greenbase
+rm -rf ./theme-greenbase/.git
+cp -R ./theme-greenbase/* ./core/
+
+# Checkout the Bootstrap Theme and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Theme-Bootstrap.git ./theme-bootstrap
+rm -rf ./theme-bootstrap/.git
+cp -R ./theme-bootstrap/* ./core/
+
+# Checkout the Reminders Plugin and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Plugin-Reminders.git ./plugin-reminders
+rm -rf ./plugin-reminders/.git
+cp -R ./plugin-reminders/* ./core/
+
+# Checkout the Wikilinks Plugin and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Plugin-Wikilinks.git ./plugin-wikilinks
+rm -rf ./plugin-wikilinks/.git
+cp -R ./plugin-wikilinks/* ./core/
+
+# Checkout the i18n Plugin and copy to the core
+git clone https://github.com/Project-Pier/ProjectPier-Plugin-i18n.git ./plugin-i18n
+rm -rf ./plugin-i18n/.git
+cp -R ./plugin-i18n/* ./core/
+
+cd ./core
+date -u > build_id.txt
+echo Testing configuration, contains plugins and themes not included in the release configuration. >> build_id.txt
+echo This configuration is for testing only and should not be used for production systems as it contains
+echo plugins, themes and tools that are known to be buggy and/or only meant for development. >> build_id.txt
+echo Jenkins Build Number $BUILD_NUMBER >> build_id.txt
+zip -r ../pkg/projectpier-master-everything-latest.zip ./*
